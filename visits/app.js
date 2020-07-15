1ecce785c0f3d096f62d1e020c3accaeb48d5c3d
Vijay Praveen <vpraveen@illumina.com>
2020-07-15 18:29:11 +0800

    remove redundant comments

--- visits/app.js
+++ visits/app.js
@@ -1,13 +1,12 @@
 'use strict';
 
+// Constants
 const express = require('express');
 const redis = require('redis');
 const ip = require('ip');
 const path = require('path');
 const router = express.Router();
 
-
-// Constants
 const HOST = '0.0.0.0';
 const PORT = 8080;
 const REDIS_PORT = '6379';
@@ -29,23 +28,13 @@ const app = express();
 app.set("view engine", "pug");
 app.set("views", path.join(__dirname, "views"));
 router.get("/", (req, res) => {
-  //res.render("index");
   client.incr('count', function(err, reply) {
   console.log(reply);
     res.render("index", { message: "This is the visitor count: " + reply, ip: "Container IP: " + ip.address() });
   });
-  //res.render("index", { count: "Hello there!" });
 });
-/*
-app.get('/', (req, res) => {
-  client.incr('count', function(err, reply) {
-    console.log(reply);
-      res.send("<h1>This is the visitor>" + reply + "</h1>");
-    });
-});
-*/
+
 //add the router
 app.use('/', router);
 app.listen(PORT, HOST);
 console.log("Container IP: " +  ip.address());
-// console.log(`Running on http://${HOST}:${PORT}`);
